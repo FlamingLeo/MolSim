576f23a7fa094f56cf7bd04a66d36dd948c36cbe
scfl <flavius.schmidt@tum.de>
2024-10-24 00:46:35 +0200

    fixed verlet header comments

--- src/simulations/Verlet.h
+++ src/simulations/Verlet.h
@@ -12,55 +12,36 @@
 #include "Simulation.h"
 #include <list>
 
+/// @brief Class containing the functionality and parameters required to run a simulation of the Velocity-Störmer-Verlet method.
 class Verlet : public Simulation
 {
 private:
+    /// @brief The list of particles used in the simulation.
     std::list<Particle> m_particles; // TODO ParticleContainer...
+    /// @brief The start time of the simulation.
     double m_start_time;
+    /// @brief The end time of the simulation.
     double m_end_time;
+    /// @brief The timestep used during the simulation.
     double m_delta_t;
+    /// @brief The output (logging) frequency for generating a file output.
     int m_it_freq;
 
 public:
     Verlet(const Arguments &args);
     ~Verlet();
 
-    /**
-     * @brief Calculates the force for all particles.
-     *
-     */
+    /// @brief Calculates the force for all particles.
     void calculateF();
 
-    /**
-     * @brief Calculates the position for all particles.
-     *
-     */
+    /// @brief Calculates the position for all particles.
     void calculateX();
 
-    /**
-     * @brief Calculates the velocity for all particles.
-     *
-     */
+    /// @brief Calculates the velocity for all particles.
     void calculateV();
 
-    /**
-     * @brief Runs the simulation.
-     *
-     */
+    /// @brief Runs the simulation.
     void runSimulation() override;
 
-    /**
-     * @brief Plots the particles.
-     *
-     * @param iteration
-     */
-    void plotParticles(int iteration);
-
-    /**
-     * @brief Get the Particles object.
-     *
-     * @return std::list<Particle>
-     */
     std::list<Particle> &getParticles();
-
 };
