64d24e85d0a0b0d7c47ccae7204f41aefbd0d970
scfl <flavius.schmidt@tum.de>
2024-11-20 21:28:17 +0100

    rewrote pair iterator, again

--- src/strategies/F.cpp
+++ src/strategies/F.cpp
@@ -17,27 +17,23 @@ void calculateF_Gravity(ParticleContainer &particles, double, double) {
 
 void calculateF_GravityThirdLaw(ParticleContainer &particles, double, double) {
     // loop over unique pairs
-    for (size_t i = 0; i < particles.size(); ++i) {
-        auto &p1 = particles[i];
+    for (auto pair = particles.beginPairs(); pair != particles.endPairs(); ++pair) {
+        auto &p1 = (*pair).first;
+        auto &p2 = (*pair).second;
 
-        // avoid recalculating force for pairs which have already been computed
-        for (size_t j = i + 1; j < particles.size(); ++j) {
-            auto &p2 = particles[j];
+        auto distVec = p2.getX() - p1.getX();
+        double distNorm = ArrayUtils::L2Norm(distVec);
 
-            auto distVec = p2.getX() - p1.getX();
-            double distNorm = ArrayUtils::L2Norm(distVec);
+        // prevent division by 0
+        if (distNorm == 0)
+            continue;
 
-            // prevent division by 0
-            if (distNorm == 0)
-                continue;
+        double forceMag = p1.getM() * p2.getM() / std::pow(distNorm, 3);
+        auto forceVec = ArrayUtils::elementWiseScalarOp(forceMag, distVec, std::multiplies<>());
 
-            double forceMag = p1.getM() * p2.getM() / std::pow(distNorm, 3);
-            auto forceVec = ArrayUtils::elementWiseScalarOp(forceMag, distVec, std::multiplies<>());
-
-            // use newton's third law to apply force on p1 and opposite force on p2
-            p1.setF(p1.getF() + forceVec);
-            p2.setF(p2.getF() - forceVec);
-        }
+        // use newton's third law to apply force on p1 and opposite force on p2
+        p1.setF(p1.getF() + forceVec);
+        p2.setF(p2.getF() - forceVec);
     }
 }
 
@@ -57,25 +53,23 @@ void calculateF_LennardJones(ParticleContainer &particles, double epsilon, doubl
 
 void calculateF_LennardJonesThirdLaw(ParticleContainer &particles, double epsilon, double sigma) {
     // loop over unique pairs
-    for (size_t i = 0; i < particles.size(); ++i) {
-        auto &p1 = particles[i];
-        for (size_t j = i + 1; j < particles.size(); ++j) {
-            auto &p2 = particles[j];
+    for (auto pair = particles.beginPairs(); pair != particles.endPairs(); ++pair) {
+        auto &p1 = (*pair).first;
+        auto &p2 = (*pair).second;
 
-            auto distVec = p1.getX() - p2.getX();
-            double distNorm = ArrayUtils::L2Norm(distVec);
+        auto distVec = p1.getX() - p2.getX();
+        double distNorm = ArrayUtils::L2Norm(distVec);
 
-            // prevent division by 0
-            if (distNorm == 0)
-                continue;
+        // prevent division by 0
+        if (distNorm == 0)
+            continue;
 
-            double forceMag = (-24 * epsilon / std::pow(distNorm, 2)) *
-                              (std::pow(sigma / distNorm, 6) - 2 * std::pow(sigma / distNorm, 12));
-            auto forceVec = ArrayUtils::elementWiseScalarOp(forceMag, distVec, std::multiplies<>());
+        double forceMag = (-24 * epsilon / std::pow(distNorm, 2)) *
+                          (std::pow(sigma / distNorm, 6) - 2 * std::pow(sigma / distNorm, 12));
+        auto forceVec = ArrayUtils::elementWiseScalarOp(forceMag, distVec, std::multiplies<>());
 
-            // use newton's third law to apply force on p1 and opposite force on p2
-            p1.setF(p1.getF() + forceVec);
-            p2.setF(p2.getF() - forceVec);
-        }
+        // use newton's third law to apply force on p1 and opposite force on p2
+        p1.setF(p1.getF() + forceVec);
+        p2.setF(p2.getF() - forceVec);
     }
 }
