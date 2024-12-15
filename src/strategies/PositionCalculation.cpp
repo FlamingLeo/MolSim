21355826d13835cca331aa1fe344a51136481624
marag <mara.godeanu@tum.de>
2024-12-15 14:21:13 +0100

    changed the way gravity is added

--- src/strategies/PositionCalculation.cpp
+++ src/strategies/PositionCalculation.cpp
@@ -26,11 +26,14 @@ void calculateX_LC(ParticleContainer &particles, double delta_t, double g_grav, 
         if (!p.isActive())
             continue;
 
+        //add gravitational force
+        std::array<double, 3> gravity = {0.0, p.getM() * g_grav, 0.0};
+        p.setF(p.getF() + gravity);
+
         p.setX(p.getX() + ArrayUtils::elementWiseScalarOp(delta_t, p.getV(), std::multiplies<>()) +
                delta_t * delta_t * ArrayUtils::elementWiseScalarOp(1 / (2 * p.getM()), p.getF(), std::multiplies<>()));
-        std::array<double, 3> gravity = {0.0, p.getM() * g_grav, 0.0};
-        p.setF(gravity);
 
+        p.setF({0,0,0});
         // check particle index and potentially move it
         // if the particle somehow goes completely out of bounds, remove it to avoid issues
         int newIdx = lc->getCellIndex(p.getX());
